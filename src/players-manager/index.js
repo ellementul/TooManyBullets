c86682968211e0ebf8a010542d834cee6c68add3
ellementul <ananev.ivan.ur@ya.ru>
2023-06-20 10:51:33 +0500

    Added pinpong events

--- src/players-manager/index.js
+++ src/players-manager/index.js
@@ -1,7 +1,11 @@
 const { Member } = require('@ellementul/united-events-environment')
+const { events: { time } } = require('@ellementul/uee-timeticker')
 const startSessionEvent = require("../events/start-session")
 const readyEvent = require("../events/ready-players-manager")
+const pingEvent = require("../events/ping-players")
 
+let timeout = 0
+const MSTIMELIMIT = 500
 class PlayersManager extends Member {
   constructor() {
     super()
@@ -10,7 +14,27 @@ class PlayersManager extends Member {
   }
 
   start() {
+    this.onEvent(time, payload => this.tick(payload))
     this.send(readyEvent)
+  }
+
+  tick({ state: { mstime }}) {
+    if(mstime - timeout > MSTIMELIMIT) {
+      timeout = mstime
+      this.runOutTimeout()
+    }
+    else {
+      this.checkPlayersPong()
+    }
+  }
+  checkPlayersPong() {
+    
+  }
+
+  runOutTimeout() {
+    //Disconnect Players Without Pong
+
+    this.send(pingEvent)
   }
 }
 
