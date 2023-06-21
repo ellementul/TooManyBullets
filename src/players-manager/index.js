e597583bfd80c652fb10b34b0f0f51b6a6b9b1ea
ellementul <ananev.ivan.ur@ya.ru>
2023-06-21 11:49:39 +0500

    Added connecting players

--- src/players-manager/index.js
+++ src/players-manager/index.js
@@ -3,6 +3,7 @@ const { events: { time } } = require('@ellementul/uee-timeticker')
 const startSessionEvent = require("../events/start-session")
 const readyEvent = require("../events/ready-players-manager")
 const pingEvent = require("../events/ping-players")
+const pongEvent = require("../events/pong-players")
 
 let timeout = 0
 const MSTIMELIMIT = 500
@@ -10,12 +11,28 @@ class PlayersManager extends Member {
   constructor() {
     super()
 
-    this.onEvent(startSessionEvent, () => this.start()) // Subscribing on event
+    this._players = new Map
+
+    this.onEvent(startSessionEvent, () => this.start())
   }
 
   start() {
     this.onEvent(time, payload => this.tick(payload))
+    this.onEvent(pongEvent, payload => this.pong(payload))
     this.send(readyEvent)
+  }
+
+  pong({  playerUuid }) {
+    if(!this._players.has(playerUuid))
+      this.connectPlayer(playerUuid)
+
+    this._players.get(playerUuid).pong = true
+  }
+
+  connectPlayer(playerUuid) {
+    this._players.set(playerUuid, {
+      pong: false
+    })
   }
 
   tick({ state: { mstime }}) {
@@ -27,14 +44,40 @@ class PlayersManager extends Member {
       this.checkPlayersPong()
     }
   }
+
   checkPlayersPong() {
-    
+    let allPong = true
+    for (const [uuid, player] of this._players) {
+      allPong &= player.pong
+    }
+
+    if(allPong) {
+      this.clearPongs()
+      this.send(pingEvent)
+    }
+  }
+
+  clearPongs() {
+    for (const [uuid, player] of this._players) {
+      player.pong = false
+    }
   }
 
   runOutTimeout() {
-    //Disconnect Players Without Pong
-
+    this.clearPlayersWithoutPong()
+    console.log(this._players)
     this.send(pingEvent)
+  }
+
+  clearPlayersWithoutPong() {
+    for (const [uuid, player] of this._players) {
+      if(!player.pong)
+        this.diconnectedPlayers(uuid)
+    }
+  }
+
+  diconnectedPlayers(playerUuid) {
+    this._players.delete(playerUuid);
   }
 }
 
