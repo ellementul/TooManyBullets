0ac8c395b9ba05c05926426d3a9d5e54566325ec
ellementul <ananev.ivan.ur@ya.ru>
2023-07-01 11:13:28 +0500

    Begin characters

--- src/characters/index.js
+++ src/characters/index.js
@@ -4,6 +4,8 @@ const genUuid = Types.UUID.Def().rand
 const connectedPlayerEvent = require("../events/connected-player")
 const spawnEvent = require("../events/spawn-character")
 const readyEvent = require("../events/ready-spawned")
+const createDynamicObject = require("../events/create-dynamic-object")
+const physicUpdateEvent = require("../events/update-physic")
 
 class CharactersManager extends Member {
   constructor() {
@@ -15,6 +17,7 @@ class CharactersManager extends Member {
     
     this.onEvent(connectedPlayerEvent, payload => this.addNewCharacter(payload))
     this.onEvent(readyEvent, payload => this.spawnCharacter(payload))
+    this.onEvent(physicUpdateEvent, payload => this.physicUpdate(payload))
   }
 
   addNewCharacter({ state: playerUid }) {
@@ -28,8 +31,17 @@ class CharactersManager extends Member {
   }
 
   spawnCharacter({ characterUuid, position }) {
-    const characterShape = this._characters.get(characterUuid).spawn({ position })
-    console.log(characterShape)
+    const characterShape = this._characters.get(characterUuid).spawn({ position }).serialize()
+    this.send(createDynamicObject, {
+      state: characterShape
+    })
+  }
+
+  physicUpdate({ state }) {
+    for (const [uuid, character] of this._characters) {
+      if(character.isSpawned())
+        console.log(uuid, character)
+    }
   }
 }
 
@@ -51,10 +63,21 @@ class Character {
       return
   
     this._state = SPAWNED
+    this.position = { x, y }
 
+    return this
+  }
+
+  isSpawned() {
+    return this._state === SPAWNED
+  }
+
+  serialize() {
     const { width, height } = this.box
+    const { x, y } = this.position
     return {
       uuid: this.uuid,
+      shape: "Box",
       box: { width, height },
       position: { x, y }
     }
--- src/physic/index.js
+++ src/physic/index.js
@@ -2,6 +2,7 @@ const { Member } = require('@ellementul/united-events-environment')
 const { events: { time } } = require('@ellementul/uee-timeticker')
 const runEvent = require("../events/run-world")
 const stopEvent = require("../events/pause-world")
+const createDynamicObject = require("../events/create-dynamic-object")
 const updateEvent = require("../events/update-physic")
 
 const PAUSE = Symbol("Pause")
@@ -11,8 +12,12 @@ class Physic extends Member {
     super()
 
     this._state = PAUSE
+
+    this._dynamicObjects = new Map
+    
     this.onEvent(runEvent, () => this.run())
     this.onEvent(stopEvent, () => this.stop())
+    this.onEvent(createDynamicObject, payload => this.createDynamic(payload))
     this.onEvent(time, () => this.step())
   }
 
@@ -24,6 +29,17 @@ class Physic extends Member {
 
   stop() {
     this._state = PAUSE
+  }
+
+  createDynamic({ state: newObject }) {
+    this._dynamicObjects.set(newObject.uuid, {
+      position: {
+        x: newObject.position.x,
+        y: newObject.position.y
+      }
+    })
+
+    // console.log(this._dynamicObjects)
   }
 
   step() {
