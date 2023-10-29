fe0da627ccdd998a9ca028a25d9010755aa265c8
ellementul <ananev.ivan.ur@ya.ru>
2023-10-29 11:55:38 +0500

    Connected logging

--- src/logging.js
+++ src/logging.js
@@ -2,6 +2,7 @@ function Logging() {
   return function (payload) {
     switch(payload.message.access) {
       case "Local":
+        // console.log(payload.message)
         break
       default:
         null
